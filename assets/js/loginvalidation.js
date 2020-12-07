64b75916a97a010c1d27a8b2f5ee65825114053c
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2020-12-07 17:05:15 +0800

    login validation

---
+++ assets/js/loginvalidation.js
@@ -0,0 +1,23 @@
+
+let submitForm = document.getElementById('submitForm')
+
+
+submitForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("logusername").value;
+    let password = document.getElementById("logpass").value;
+    let error = document.getElementById("#error");
+
+    console.log(username, password)
+    if(username == "admin" && password == "admin")
+    {
+        let = baseURl = window.location.origin;
+        window.location.href = baseURl + '/admin.html';
+    } else {
+        
+        document.getElementById('erroralert').innerHTML = '<div class="alert alert-danger" role="alert">Unauthorized Access!</div>' ;
+        document.getElementById('submitForm').reset();
+    }
+
+})
